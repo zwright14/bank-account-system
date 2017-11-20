fe19c6ab9720b66a07dc66b618e57f668f656219
Zach Wright <zwright14@gmail.comm>
2017-11-20 12:49:35 -0600

    Creation of basic abstract account class

---
+++ src/Account.java
@@ -0,0 +1,51 @@
+
+public abstract class Account {
+	
+	private int accountNumber;
+	private String accountName;
+	private String accountKey;
+	private float balance;
+	
+	public Account(int num, String name, String key) {
+		accountNumber = num;
+		accountName = name;
+		accountKey = key;
+		balance = 0;
+	}
+	
+	//Getter Methods
+	public int getNumber() {
+		return accountNumber;
+	}
+	
+	public String getName() {
+		return accountName;
+	}
+	
+	public String getKey() {
+		return accountKey;
+	}
+	
+	public float getBalance() {
+		return balance;
+	}
+	
+	//Setter Methods
+	public void setName(String newName) {
+		accountName = newName;
+	}
+	
+	public void setKey(String newKey) {
+		accountKey = newKey;
+	}
+	
+	//Transactions
+	public void withdrawMoney(float withdrawal) {
+		balance -= withdrawal;
+	}
+	
+	public void depositMoney(float deposit) {
+		balance += deposit;
+	}
+
+}
