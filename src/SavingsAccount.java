3c43c680f75b4ea563c858c9ad78e9e0426b7819
Romeo Scagliarini <romeoscagliarini@yahoo.com>
2017-12-11 22:47:42 -0600

    Finalizing everything but comments.
    Yeee.

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -7,15 +7,15 @@ public class SavingsAccount extends Account {
 		super(num, name, key);
 		withdrawLimit = 6;
 	}
-	
+	//withdrawal limit getter (like in real savings accounts) 
 	public int getWithdrawLimit() {
 		return withdrawLimit; 
 	}
-	
+	//resets the withdrawal limit so you can continue to withdraw
 	public void resetWithdrawalLimit() {
 		withdrawLimit = 6;
 	}
-	
+	//checks withdraw 
 	public boolean withdrawalIsPossible(double withdrawal) {
 		if (withdrawLimit>0) {
 			return super.withdrawalIsPossible(withdrawal);
@@ -24,7 +24,7 @@ public class SavingsAccount extends Account {
 			return false;
 		}
 	}
-	
+	//withdraw from account class 
 	public void withdrawMoney(double withdrawal, String description) {
 		if (description.isEmpty()) {
 			super.withdrawMoney(withdrawal, description);
